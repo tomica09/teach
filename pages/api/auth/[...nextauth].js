48231d9fdc641d10bceabffc9a8f32cd563e121b
tomica09 <tomica09@naver.com>
2024-09-03 22:57:24 +0900

    d

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+
+export default NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID, // Rest API Key
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+});
